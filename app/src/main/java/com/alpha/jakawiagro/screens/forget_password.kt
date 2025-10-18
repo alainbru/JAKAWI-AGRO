f7219153aea598553378753fc15ae28adbc69ba2
Abad-cyber <pomamaqueraabad@gmail.com>
2025-10-18 17:52:25 -0500

    arreglando errores en los screen de inicio

--- app/src/main/java/com/alpha/jakawiagro/screens/forget_password.kt
+++ app/src/main/java/com/alpha/jakawiagro/screens/forget_password.kt
@@ -1,7 +1,6 @@
 package com.alpha.jakawiagro.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -14,7 +13,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Button
